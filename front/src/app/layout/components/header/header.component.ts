4e7ddd8bacf8b73d303426c2b539681f60a1278a
Jean-Baptiste Poujol <jbpoujol@pm.me>
2024-05-23 16:19:37 +0200

    feat: header

--- front/src/app/layout/components/header/header.component.ts
+++ front/src/app/layout/components/header/header.component.ts
@@ -1,15 +1,51 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { MenuItem } from 'primeng/api';
+import { AuthService } from 'src/app/auth/services/auth.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss'],
 })
-export class HeaderComponent {
-  constructor(private router: Router) {}
+export class HeaderComponent implements OnInit {
+  items!: MenuItem[];
 
-  isActive(route: string): boolean {
-    return this.router.url === route;
+  constructor(private router: Router, private authService: AuthService) {}
+
+  ngOnInit(): void {
+    this.items = [
+      {
+        label: 'Articles',
+        command: () => {
+          this.router.navigate(['/articles']);
+        },
+      },
+      {
+        label: 'Thèmes',
+        command: () => {
+          this.router.navigate(['/themes']);
+        },
+      },
+      {
+        icon: 'pi pi-fw pi-user',
+        items: [
+          {
+            label: 'Profile',
+            icon: 'pi pi-fw pi-user',
+            command: () => {
+              this.router.navigate(['/profile']);
+            },
+          },
+          {
+            label: 'Quit',
+            icon: 'pi pi-fw pi-power-off',
+            command: () => {
+              this.authService.logout();
+            },
+          },
+        ],
+      },
+    ];
   }
 }
